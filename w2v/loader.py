8cf0998352c5c259c0c5e17caecdd44938fdb42a
PiotrTrybala <trybalapiotr4@gmail.com>
2026-03-19 19:09:23 +0100

    implemented preprocess and loader classes

--- w2v/loader.py
+++ w2v/loader.py
@@ -1,13 +1,31 @@
+import zipfile
+
 
 class Loader(object):
     
     def __init__(self):
         pass
-    @staticmethod
-    def load_text(filename: str) -> str:
-        pass
     
+    """
+    load_zip_iterator - allows for streaming data without spiking the RAM usage
+    """
+    @staticmethod
+    def load_zip_iterator(filename: str):
+        with zipfile.ZipFile(filename) as zref:
+            target_file = next((f for f in zref.namelist() if f.endswith('.txt')))
+            
+            if not target_file:
+                raise FileNotFoundError('No valid .txt file found in zip')
+            
+            with zref.open(target_file) as f:
+                for line in f:
+                    yield line.decode('utf-8', errors='ignore').strip()
+    """
+    load_zip - load entire contents of the ZIP archive (assuming it is only one file)    
+    """
     @staticmethod
     def load_zip(filename: str) -> str:
-        content = ""
-        return content
+        with zipfile.ZipFile(filename) as zref:
+            target_file = next((f for f in zref.namelist() if not f.startswith('__')), zref.namelist()[0])
+            return zref.read(target_file).decode('utf-8', errors='ignore').replace('\n', ' ')
+            
--- w2v/preprocess.py
+++ w2v/preprocess.py
@@ -1,9 +1,64 @@
+import string
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
 
-def preprocess(corpus):
-    training_data = []
+"""
+preprocess - split sentences into words and remove all the stopwords like the, is, in etc.
+and store them into lists
+"""
+
+def preprocess(corpus: str) -> list[str]:
+    
+    training_data: list[str] = []
+    stop_words = set(stopwords.words('english'))
+    
+    sentences = corpus.split('.')
+    
+    for i in range(len(sentences)):
+        
+        sentences[i] = sentences[i].strip()
+        sentence = sentences[i].split()
+        
+        x = [word.strip(string.punctuation) for word in sentence if word not in stop_words]
+        x = [word.lower() for word in x]
+        training_data.append(x)
+    
     return training_data
 
-def prepare_training(sentences):
+"""
+prepare_training - used for preparing two lists: one is target word, and the other is for
+all context that applies to it
+
+"""
+
+def prepare_training(window_size: int, sentences: list[list[str]]):
     data = {}
-    X_train, y_train = [], []
-    return X_train, y_train
+    
+    for sentence in sentences:
+        for word in sentence:
+            if word not in data:
+                data[word] = 1
+            else:
+                data[word] += 1
+    
+    vocabulary_size = len(data)
+    vocabulary = {}
+    
+    for i in range(len(data)):
+        vocabulary[data[i]] = i
+        
+    x_train, y_train = [], []
+    for sentence in sentences:
+        for i in range(len(sentence)):
+            center_word = [0 for _ in range(vocabulary_size)]
+            center_word[vocabulary[sentence[i]]] = i
+            context = [0 for _ in range(vocabulary_size)]
+            
+            for j in range(i - window_size, i + window_size):
+                if i != j and j >= 0 and j < len(sentences):
+                    context[vocabulary[sentences[j]]] += 1
+                    x_train.append(center_word)
+                    y_train.append(context)
+
+    return vocabulary, vocabulary_size, x_train, y_train
